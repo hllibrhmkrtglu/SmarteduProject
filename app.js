9901127dfe182ad9d0cdb82fa6e21dac8390514a
Your Name <you@example.com>
2023-08-14 14:23:44 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express(); // express fonksiyonunu çağırarak nesne oluşturuluyor
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 const port = 3000;
 
+// Routes
 app.get('/', (req, res) => {
-    res.send('Merhaba, Express!');
+    res.status(200).render("index", { page_name: "index" });
 });
+app.get('/about', (req, res) => {
+    res.render('about', { page_name: 'about' }); // Sayfa adını veri olarak iletiyoruz
+});
+
+
+
 
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatıldı`);
